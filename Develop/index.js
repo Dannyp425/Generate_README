54daf6df969c7f9cd6acfb1fa502418b7d3def99
Danny <dannyp425@gmail.com>
2021-03-19 20:31:53 -0600

    starter code

---
+++ Develop/index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is the title of your project?",
+    "What is a consice descriptionpf your project?",
+    
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
