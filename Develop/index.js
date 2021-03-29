260113d2785e52056b0b674dfff1514aa64d3b57
Danny <dannyp425@gmail.com>
2021-03-28 22:18:31 -0600

    Everything but the GitHub link

--- Develop/index.js
+++ Develop/index.js
@@ -1,17 +1,61 @@
+
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+let generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is the title of your project?",
-    "What is a consice descriptionpf your project?",
-    
+    {   type: "input",
+        name: "title",
+        message: "What is the title of your project?"   },
+    {   type: "input",
+        name: "description",
+        message: "What is a consice description of your project?"   },
+    {   type: "input",
+        name: "installation",
+        message: "Provide a step-by-step description of how to get the development environment running."   },
+    {   type: "input",
+        name: "usage",
+        message: "What is this project used for?"   },
+    {   type: "list",
+        name: "licenses",
+        message: "Are there any licenses you want to add to this project?",
+        choices: ["MIT", "MPL", "Eclipse", "N/A"]   },
+    {   type: "input",
+        name: "contribution",
+        message: "What are the contribution guidelines(if any)?"   },
+    {   type: "input",
+        name: "test",
+        message: "How many tests do you have and what instructions do you have for them?"},
+    {   type: "input",
+        name: "questions",
+        message: "What is your email?"},
+    {   type: "input",
+        name: "profile",
+        message: "What is your GitHub username?"}
 ];
 
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let markDown = generateMarkdown(data);
+    fs.writeFile(fileName, markDown, (err) => {
+        if(err){
+            console.log(err);
+        } else{
+            console.log("ReadMe has been created succefully!");
+        }
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+            .then(answers => {
+                writeToFile("ReadMe.md", answers);
+            });
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT"){
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "Eclipse") {
+    return "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+  } else if (license === "MPL") {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  } else {
+    return " ";
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +22,43 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let badge = renderLicenseBadge(data.licenses);
+  return `
+  ${badge}
+  # ${data.title}
 
+  ## **Description**:
+    ${data.description}
+
+  ## Table of Content
+  * [Get started](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributiion](#contributing)
+  * [Test(s)](#test(s))
+  * [Profile](#profile)
+
+  ### **Installation**:
+    ${data.installation}
+
+  ### **Usage**:
+    ${data.usage}
+
+  ### **Licenses**:
+    ${data.licenses}
+
+  ### **Contributing**:
+    ${data.contribution}
+    
+  ### **Test(s)**:
+    ${data.test}
+
+  ### **Questions**:
+    Please refer all questions to ${data.questions}.
+
+  ### **Profile**:
+    You can also visit my GitHub at [github.com/${data.profile}](https://www.github.com/Dannyp425)
 `;
-}
+};
 
 module.exports = generateMarkdown;
